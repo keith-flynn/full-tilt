cd429d760ab04ffa39b72da65aeb4606c5fe8f5e
Keith Flynn <flynn.keith@gmail.com>
2023-04-30 14:02:03 -0400

    template for knight objects

---
+++ script.js
@@ -0,0 +1,25 @@
+const knight1 = {
+    m: 4,
+    ws: 5,
+    bs: 5,
+    s: 4,
+    t: 4,
+    w: 2,
+    i: 5,
+    a: 3,
+    ld: 8,
+    save: "4+"
+}
+
+const knight2 = {
+    movement: 4,
+    weaponSkill: 5,
+    ballisticSkill: 5,
+    strength: 4,
+    toughness: 4,
+    wounds: 2,
+    initiative: 5,
+    attacks: 3,
+    leadership: 8,
+    armorSave: "4+"
+}
